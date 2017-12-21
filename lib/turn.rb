d52781dbabdeb763a214cbfb1c2846c359b590c1
abdelrauof mohammed <abdelrauof97@gmail.com>
2017-12-21 22:12:16 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def position_taken?(board, index)
+  if "#{board[index]}" == "" || "#{board[index]}" == " " || "#{board[index]}" == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, "#{index}".to_i) || !(("#{index}".to_i).between?(0, 8))
+    return false
+  else
+    return true
+  end
+end
+
+def turn(board)
+  if !("#{index}".to_i.between?(1- 9))
+      puts "Please enter" 1-9:"
+      input = gets.strip
+      index = input.to_i - 1
+      valid_move?(board, "#{index}".to_i)
+  end
+      
+end
+
+def move(board, index, value)
+  board[index] =value
+end
